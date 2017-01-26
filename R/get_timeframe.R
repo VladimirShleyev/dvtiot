248874de94120ae82fb19fbf46b607da7ac96c2c
iMissile <ishutov@gmail.com>
2017-01-26 11:52:21 +0300

    Начал выделять отдельные фунции

---
+++ R/get_timeframe.R
@@ -0,0 +1,14 @@
+#' Get appropriate timeframe window from current time.
+#' Timeframe adjusted up to day unit and has forward and backward extension.
+
+getTimeframe <- function(days_back = 7, days_forward = 3){
+  # если по каким-либо причинам наверху не определились с прогнозом (NA),
+  # то полагаем что он есть и он равен базовому горизонту
+  days_formard <- ifelse(is.na(days_forward), 3, days_forward)
+  min_lim <- floor_date(now() - days(days_back), unit = "day")
+  # поскольку будущее округляем вниз, то надо добавить еще сутки (+1)
+  max_lim <- ceiling_date(now() + days(days_forward), unit = "day")
+  timeframe <- c(min_lim, max_lim)
+
+  timeframe
+}
---
+++ R/time_groups.R
@@ -0,0 +1,34 @@
+#' Different functions to group measurements with seconds resolution
+#' into high ordered time intervals
+
+hgroup.enum0 <- function(date, time.bin = 4){
+  # привязываем все измерения, которые попали в промежуток +-1/2 интервала, к точке измерения.
+  # точки измерения могут быть кратны 1, 2, 3, 4, 6, 12 часам, определяется time.bin
+  # отсчет измерений идет с 0:00
+  tick_time <- date + minutes(time.bin * 60)/2 # сдвигаем на пол интервала вперед
+  n <- floor(hour(tick_time) / time.bin)
+  floor_date(tick_time, unit = "day") + hours(n * time.bin)
+}
+
+hgroup.enum1 <- function(date, time.bin = 4){
+  # привязываем все измерения, которые попали в промежуток [0, t] к точке измерения.
+  # точки измерения могут быть кратны 1, 2, 3, 4, 6, 12 часам, определяется time.bin
+  # отсчет измерений идет с 0:00
+  tick_time <- date
+  n <- floor(hour(tick_time) / time.bin)
+  floor_date(tick_time, unit = "day") + hours(n * time.bin)
+}
+
+hgroup.enum <- function(date, time.bin = 4){
+  # привязываем все измерения, которые попали в промежуток [0, t] к точке измерения.
+  # точки измерения могут быть кратны 1, 2, 3, 4, 6, 12 часам, определяется time.bin
+  # отсчет измерений идет с 0:00
+
+  # поправка для лаборатории. для группировки меньше часа допускается указывать числа меньше 1
+  # 0.5 -- раз в полчаса.0.25 -- раз в 15 минут
+
+  tick_time <- date
+  if (time.bin < 1 & !(time.bin %in% c(0.25, 0.5))) time.bin = 1
+  n <- floor((hour(tick_time)*60 + minute(tick_time))/ (time.bin * 60))
+  floor_date(tick_time, unit = "day") + minutes(n * time.bin *60)
+}
